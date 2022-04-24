263ee3e760291c92fa64a590307998c8127b4b83
Sergey Kotelevsky <sergey.k@revered.pro>
2022-04-24 23:36:30 +0300

    Создание постов

---
+++ src/components/LoadImage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="load-img">
+        <div class="load-img-remove">
+            <i class="fas fa-remove"></i>
+        </div>
+        <img :src="image.src" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoadImage",
+    props: ['image'],
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/post/CreatePostPage.vue
+++ src/pages/post/CreatePostPage.vue
@@ -8,25 +8,58 @@
             </div>
 
             <div class="mt-2 text-right">
+                <button @click="openLoadImg"
+                        type="button"
+                        class="btn btn-primary p-2 mr-2"
+                >
+                    <i class="fas fa-image"></i>
+                    <input @change="addImage" ref="loadInput" type="file" hidden/>
+                </button>
                 <button class="btn btn-primary"
                         type="submit"
                 >Добавить</button>
             </div>
         </form>
+        <LoadImage v-for="(image, index) in images"
+                   :image="image"
+                   :key="index"
+        />
     </section>
 </template>
 
 <script>
+import LoadImage from "@/components/LoadImage";
+import api from "@/api";
 export default {
     name: "CreatePostPage",
+    components: {LoadImage},
     data(){
         return {
             post: '',
+            images: [],
         }
     },
     methods: {
+        openLoadImg(){
+            this.$refs.loadInput.click();
+        },
+        async addImage(){
+            const file = this.$refs.loadInput.files[0];
+            const imageBase64 = await this.loadImage(file);
+            this.images.push({ src: imageBase64 });
+        },
         addPost(){
-            console.log(this.post);
+            //console.log(this.post);
+            api.post('/post/create', { post: this.post, images: this.images });
+        },
+        loadImage(file){
+            return new Promise((resolve) => {
+                const reader = new FileReader();
+                reader.onloadend = function() {
+                    resolve(reader.result);
+                };
+                reader.readAsDataURL(file);
+            });
         }
     }
 }
